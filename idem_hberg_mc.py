cc533fb78d85a0794cec01db6bc0e26ebab29bcd
dpaj <daniel@pajerowski.com>
2017-11-16 15:31:42 -0500

    separating module from run

--- idem_hberg_mc.py
+++ idem_hberg_mc.py
@@ -961,24 +961,3 @@ def nn_pair_corr(i,j,k):
 
 	
 	
-print(time()-start_time)
-#type 0 = Fe
-#type 1 = Mn
-	
-my_lattice = SpinLattice(\
-iron_doping_level=0.0, edge_length = 24, s_max_0 = 2.5, s_max_1 = 2.0, \
-single_ion_anisotropy_0 = np.array([0,0,-0.01]), single_ion_anisotropy_1 = np.array([-4.0,0,0.0]), superexchange = -1, \
-magnetic_field = np.array([0,0,0]))
-my_lattice.init_arrays()
-my_lattice.make_op_masks()
-my_lattice.bond_list_calc()
-
-
-print(time()-start_time)
-my_lattice.random_ijk_list_generator()
-
-my_lattice.temperature_sweep(temperature_max=201.0, temperature_min=1.0, temperature_steps=21, \
-equilibration_steps=30, number_of_angle_states=100, magnetic_field=np.array([0.0,0.0,0.0]))
-
-print('\ntime=', time()-start_time)
-exit()
