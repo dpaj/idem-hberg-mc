7f34b73741f72c9f75d87dcf8df956bcd935bb28
Daniel Pajerowski <daniel@pajerowski.com>
2018-01-11 23:05:16 -0500

    multiple submits setup test

--- AMnFeO3_multirun_linux_terminal.py
+++ AMnFeO3_multirun_linux_terminal.py
@@ -1,5 +1,8 @@
 from __future__ import print_function
 from idem_hberg_mc import *
+import os
+import subprocess
+import numpy as np
 
 """			
 JFeFeb = self.superexchange[0]
@@ -30,7 +33,17 @@ class ParameterList(object):
 		self.DFe_len = DFe_len
 		self.DFe_hat = DFe_hat
 	def return_superexchange_list(self):
-		return [La_paper.JFeFeb, La_paper.JFeFeac, La_paper.JMnMnb, La_paper.JMnMnac, La_paper.JMnFeb, La_paper.JMnFeac]
+		return [self.JFeFeb, self.JFeFeac, self.JMnMnb, self.JMnMnac, self.JMnFeb, self.JMnFeac]
+	def return_superexchange_list_MnFeA(self):
+		return [self.JFeFeb, self.JFeFeac, self.JMnMnb, self.JMnMnac, self.JMnFeb, -self.JMnFeac]
+	def return_superexchange_list_MnFeC(self):
+		return [self.JFeFeb, self.JFeFeac, self.JMnMnb, self.JMnMnac, -self.JMnFeb, self.JMnFeac]
+	def return_superexchange_list_MnFeB(self):
+		return [self.JFeFeb, self.JFeFeac, self.JMnMnb, self.JMnMnac, -self.JMnFeb, -self.JMnFeac]
+	def return_superexchange_list_MnFeG(self):
+		return [self.JFeFeb, self.JFeFeac, self.JMnMnb, self.JMnMnac, self.JMnFeb, self.JMnFeac]
+	def return_superexchange_list_MnFe0(self):
+		return [self.JFeFeb, self.JFeFeac, self.JMnMnb, self.JMnMnac, 0.0,0.0]
 	def normalize_hat(self):
 		DFe_hat = self.DFe_hat
 		DMn_hat = self.DMn_hat
@@ -46,6 +59,67 @@ class ParameterList(object):
 def make_parameter_list():
     superexchange = ParameterList()
     return superexchange
+	
+
+def write_and_run_file(iron_doping_level, edge_length, \
+single_ion_anisotropy_len_0, single_ion_anisotropy_len_1, single_ion_anisotropy_hat_0, single_ion_anisotropy_hat_1, \
+superexchange_list, \
+file_prefix, \
+anisotropy_symmetry, \
+temperature_max, temperature_min, temperature_steps, equilibration_steps):
+	iron_doping_level = str(iron_doping_level)
+	edge_length = str(edge_length)
+	single_ion_anisotropy_len_0 = str(single_ion_anisotropy_len_0)
+	single_ion_anisotropy_len_1 = str(single_ion_anisotropy_len_1)
+	single_ion_anisotropy_hat_0 = np.array2string(single_ion_anisotropy_hat_0, separator=', ')
+	single_ion_anisotropy_hat_1 = np.array2string(single_ion_anisotropy_hat_1, separator=', ')
+	superexchange_list = str(superexchange_list)
+	file_prefix = '"'+str(file_prefix)+'"'
+	anisotropy_symmetry = '"'+str(anisotropy_symmetry)+'"'
+	temperature_max = str(temperature_max)
+	temperature_min = str(temperature_min)
+	temperature_steps = str(temperature_steps)
+	equilibration_steps = str(equilibration_steps)
+
+	f = open("dummy.py", 'w')
+	f.write('from idem_hberg_mc import *')
+	f.write('\n')
+	f.write('#type 0 = Fe\n')
+	f.write('#type 1 = Mn\n')
+	f.write('#order of superexchange list\n')
+	f.write('# JFeFeb, JFeFeac, JMnMnb, JMnMnac, JMnFeb, JMnFeac\n')
+	f.write('#to use the hardcoded DFT values for La, just put -1\n')
+	f.write('#to use the hardcoded DFT values for Nd, just put -2\n')
+	f.write('#to use the experimental values for La and old DFT for MnFe, just put -3\n')
+	f.write('#to use the hardcoded extrapolated values for La, just put -4\n')
+	f.write('#to use the hardcoded extrapolated values for Nd, just put -5\n')
+	f.write('#REMEMBER THE SIGN CONVENTION FOR J, here the J>0 is antiferro J< is ferro\n')
+	f.write('\n')
+	f.write('\n')
+	f.write('my_lattice = SpinLattice(\\\n')
+	f.write('iron_doping_level='+iron_doping_level+', edge_length = '+edge_length+', s_max_0 = 2.5, s_max_1 = 2.0, \\\n')
+	f.write('single_ion_anisotropy_len_0 = '+single_ion_anisotropy_len_0+', single_ion_anisotropy_len_1 = '+single_ion_anisotropy_len_1+', \\\n')
+	f.write('single_ion_anisotropy_hat_0 = '+single_ion_anisotropy_hat_0+', single_ion_anisotropy_hat_1 = '+single_ion_anisotropy_hat_1+', \\\n')
+	f.write('superexchange = '+superexchange_list+', \\\n')
+	f.write('magnetic_field = np.array([0,0,0]), file_prefix = '+file_prefix+', anisotropy_symmetry = '+anisotropy_symmetry+')\n')
+	f.write('if 1:\n')
+	f.write('	#this is the solid solution\n')
+	f.write('	my_lattice.init_arrays()\n')
+	f.write('if 0:\n')
+	f.write('	#this is the double perovskite\n')
+	f.write('	my_lattice.init_arrays_double_perovskite()\n')
+	f.write('my_lattice.make_op_masks()\n')
+	f.write('my_lattice.bond_list_calc()\n')
+	f.write('\n')
+	f.write('\n')
+	f.write('my_lattice.random_ijk_list_generator()\n')
+	f.write('\n')
+	f.write('my_lattice.temperature_sweep(temperature_max='+temperature_max+', temperature_min='+temperature_min+', temperature_steps='+temperature_steps+', \\\n')
+	f.write('equilibration_steps='+equilibration_steps+', number_of_angle_states=100, magnetic_field=np.array([0.0,0.0,0.0]))\n')
+	f.close()
+	print('subprocess.call(r"nohup python dummy.py & disown", shell=True, stdout=subprocess.PIPE)') ###LATER THIS WILL BE AN OS COMMAND WITHOUT THE PRINT
+	###???proc = subprocess.call(r"aomx.exe <dummy.in >dummy.out", shell=True, stdout=subprocess.PIPE)
+
 
 #La values from my paper
 La_paper = make_parameter_list()
@@ -79,51 +153,37 @@ print(Nd_paper.DMn_len)
 superexchange_list = La_paper.return_superexchange_list()
 print(superexchange_list)
 
+single_ion_anisotropy_len_0, single_ion_anisotropy_len_1, single_ion_anisotropy_hat_0, single_ion_anisotropy_hat_1 = La_paper.DFe_len, La_paper.DMn_len, La_paper.DFe_hat, La_paper.DMn_hat
 
-temperature_max=200
-temperature_min=1.0
-temperature_steps=51
+temperature_steps=81
 equilibration_steps=200
 number_of_angle_states=100
-magnetic_field=np.array([0.0,0.0,0.0]))
-edge_length = 6
+magnetic_field=np.array([0.0,0.0,0.0])
+edge_length = 10
 
-x_values_to_run = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
-max_temperatures_to_run = [
-min_temperatures_to_run
+anisotropy_symmetry = "Pnma"
 
-exit()
+TN_LMO = 138.0
+TN_NMO = 82.0
+TN_LFO = 738.0
+TN_NFO = 689.0
 
-#type 0 = Fe
-#type 1 = Mn
-#order of superexchange list
-# JFeFeb, JFeFeac, JMnMnb, JMnMnac, JMnFeb, JMnFeac
-#to use the hardcoded DFT values for La, just put -1
-#to use the hardcoded DFT values for Nd, just put -2
-#to use the experimental values for La and old DFT for MnFe, just put -3
-#to use the hardcoded extrapolated values for La, just put -4
-#to use the hardcoded extrapolated values for Nd, just put -5
-#REMEMBER THE SIGN CONVENTION FOR J, here the J>0 is antiferro J< is ferro
+x_values_to_run = np.linspace(0,1,11)
+max_temperatures_to_run = np.linspace(TN_LMO+50, TN_LFO+50,11)
+min_temperatures_to_run = np.ones(11)
 
-	
-my_lattice = SpinLattice(\
-iron_doping_level=0.0, edge_length = 6, s_max_0 = 2.5, s_max_1 = 2.0, \
-single_ion_anisotropy_len_0 = D_0, single_ion_anisotropy_len_1 = D_1, \
-single_ion_anisotropy_hat_0 = D_0_hat, single_ion_anisotropy_hat_1 = D_0_hat, \
-superexchange = superexchange_list, \
-magnetic_field = np.array([0,0,0]), file_prefix = "NMFO_D_0_DubPerov_", anisotropy_symmetry = "Pnma")
-if 1:
-	#this is the solid solution
-	my_lattice.init_arrays()
-if 0:
-	#this is the double perovskite
-	my_lattice.init_arrays_double_perovskite()
-my_lattice.make_op_masks()
-my_lattice.bond_list_calc()
+print(x_values_to_run, max_temperatures_to_run, min_temperatures_to_run)
 
+file_prefix_list = ["LMFO_MnFe=A_", "LMFO_MnFe=B_", "LMFO_MnFe=C_", "LMFO_MnFe=G_", "LMFO_MnFe=0_"]
+for sx_fn_idx, superexchange_function in enumerate([La_paper.return_superexchange_list_MnFeA, La_paper.return_superexchange_list_MnFeB, La_paper.return_superexchange_list_MnFeC, La_paper.return_superexchange_list_MnFeG, La_paper.return_superexchange_list_MnFe0]):
+	superexchange_list = superexchange_function()
+	file_prefix = file_prefix_list[sx_fn_idx]
+	for idx, iron_doping_level in enumerate(x_values_to_run):
+		write_and_run_file(iron_doping_level, edge_length, \
+		single_ion_anisotropy_len_0, single_ion_anisotropy_len_1, single_ion_anisotropy_hat_0, single_ion_anisotropy_hat_1, \
+		superexchange_list, \
+		file_prefix, \
+		anisotropy_symmetry, \
+		max_temperatures_to_run[idx], min_temperatures_to_run[idx], temperature_steps, equilibration_steps)
 
-my_lattice.random_ijk_list_generator()
-
-my_lattice.temperature_sweep(temperature_max=200, temperature_min=1.0, temperature_steps=51, \
-equilibration_steps=150, number_of_angle_states=100, magnetic_field=np.array([0.0,0.0,0.0]))
 
