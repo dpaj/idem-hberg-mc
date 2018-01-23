fixed superexchange scaling

fitting_TN_pure/LMO_fitTN_multirun_linux_terminal.py
@@ -191,7 +191,7 @@ for i in J_scale_factor_list:
 max_temperature_to_run = 200.0
 min_temperature_to_run = 100.0
 for J_s_f_idx, J_scale_factor in enumerate(J_scale_factor_list):
-	np.multiply(J_scale_factor, superexchange_list)
+	superexchange_list = np.multiply(J_scale_factor, superexchange_list)
 	file_prefix = file_prefix_list[J_s_f_idx]
 	write_and_run_file(iron_doping_level, edge_length, \
 	single_ion_anisotropy_len_0, single_ion_anisotropy_len_1, single_ion_anisotropy_hat_0, single_ion_anisotropy_hat_1, \